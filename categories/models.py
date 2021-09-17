0483a910274f1af6fdcd35fd3b19f7163a6f0c82
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-17 22:43:28 +0100

    Added Category model and registered in admin site

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    slug = models.CharField(max_length=100, blank=False, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    image = models.ImageField(upload_to='photos/categories', blank=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
