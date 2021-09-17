15e181efad3969df28d65facc0c7785d29e3349b
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-18 00:38:14 +0100

    Added functionality to dynamically render products in index.html

--- GreatKart/views.py
+++ GreatKart/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from store.models import Product
 
 
 def home(request):
-    """ Home view to render index.html template """
-    return render(request, 'index.html')
+    """ Home view to render index.html template and return all active products """
+    products = Product.objects.filter(is_available=True)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'index.html', context)
