53bbce974f8f626411ff90a7276df6910a3970b3
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-18 00:57:40 +0100

    Created URLs and basic store view to render store.html

--- GreatKart/urls.py
+++ GreatKart/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('store/', include('store.urls'))
 ]
 
 urlpatterns += static(
