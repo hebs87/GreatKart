308c06696b9ac277764264de82a016a1302fda3f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-18 00:21:03 +0100

    Created Product model and ProductAdmin, registered both in admin site

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ('name', 'price', 'stock', 'category', 'modified', 'is_available')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from categories.models import Category
+
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.FloatField()
+    images = models.ImageField(upload_to='photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        if len(self.name) > 50:
+            return f'{self.name[:50]}...'
+
+        return self.name
