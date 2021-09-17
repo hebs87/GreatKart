d33431e0405ec692867bda70f91125862a53e02a
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-18 00:05:18 +0100

    Installed and configured django-cleanup

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_cleanup',
+
     'accounts.apps.AccountsConfig',
     'categories.apps.CategoriesConfig',
 ]
