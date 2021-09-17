f6faee9044981adfb3f6b9dfe2d1afbfe1266f22
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-17 22:40:01 +0100

    Configured media settings

--- GreatKart/urls.py
+++ GreatKart/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
