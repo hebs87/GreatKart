34a0eae4c48fafeae90c3e3b27dd5b270cfbcf24
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-09-18 01:21:21 +0100

    Created products_by_category url and update store view to return filtered products

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,5 @@ from .views import store
 
 urlpatterns = [
     path('', store, name='store'),
+    path('<slug:category_slug>/', store, name='products_by_category'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from .models import Product
+from categories.models import Category
 
 
 # Create your views here.
-def store(request):
-    """ A view to render the products in the store.html template """
-    products = Product.objects.filter(is_available=True)
+def store(request, category_slug=None):
+    """ A view to render the products in the store.html template - allow filtering by category """
+    category = None
+    products = None
+
+    if category_slug is not None:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category, is_available=True)
+    else:
+        products = Product.objects.filter(is_available=True)
+
     product_count = products.count()
 
     context = {
